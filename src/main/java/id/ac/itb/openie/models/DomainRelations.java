7623cad28373ebcb032971b59dfbd5675eea3d13
irfanariq <13515112@std.stei.itb.ac.id>
2019-05-16 06:10:51 +0700

    implement constructor from file for domain relations

--- src/main/java/id/ac/itb/openie/models/DomainRelations.java
+++ src/main/java/id/ac/itb/openie/models/DomainRelations.java
@@ -1,7 +1,12 @@
 package id.ac.itb.openie.models;
 
+import id.ac.itb.openie.utils.Utilities;
+
 import java.io.File;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class DomainRelations {
     private ArrayList<DomainRelation> domainRelations = new ArrayList<>();
@@ -11,6 +16,24 @@ public class DomainRelations {
 
     public DomainRelations(File file) {
         // TODO Implement this method
+        String relationsString = Utilities.getFileContent(file);
+
+        Pattern pola = Pattern.compile("openIERelation\\(Source:\\s(.*)\\nKalimat ke-(\\d*?):\\s(.*)\\nRelasi:\\s(.*?)\\((.*)#\\s(.*)\\)\\n\\)\\s\\nrelasi\\((.*)\\)\\s\\nargumen\\((.*)\\)\\s\\n");
+
+        Matcher m = pola.matcher(relationsString);
+
+        while (m.find()) {
+            Relation openIERelation = new Relation(m.group(5), m.group(4), m.group(6), m.group(1),
+                    Integer.valueOf(m.group(2)) - 1, m.group(3));
+
+            DomainRelation domainRelation = new DomainRelation();
+            domainRelation.setRelasi(m.group(7));
+            ArrayList<String> domainArgmens = new ArrayList<>(Arrays.asList(m.group(8).split("#")));
+            domainRelation.setArgumen(domainArgmens);
+            domainRelation.setOpenIERelation(openIERelation);
+
+            this.domainRelations.add(domainRelation);
+        }
     }
 
     @Override
